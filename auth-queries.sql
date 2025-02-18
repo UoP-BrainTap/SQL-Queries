2fd77f2b67133f37a1d7850a26c8d864d0731e8f
Benjamin Lovering <benjaminlovering1@gmail.com>
2025-02-18 20:14:20 +0000

    More auth changes

--- auth-queries.sql
+++ auth-queries.sql
@@ -1,6 +1,16 @@
 -- check if user exists
 SELECT EXISTS(SELECT 1 FROM users WHERE email = 'test@gmail.com') AS exists;
 
+-- check if user exists and if not create a new user
+-- if 0 rows returned the user already exists, if an id is returned the user was created
+WITH existing_user AS (
+    SELECT id, email FROM users WHERE email = 'test@example.com'
+)
+INSERT INTO users (email, password_hash, role)
+SELECT 'test@example.com', 'hash', 'lecturer'
+WHERE NOT EXISTS (SELECT 1 FROM existing_user)
+RETURNING id;
+
 -- check if user exists and password is correct
 SELECT EXISTS(SELECT 1 FROM users WHERE email = 'test@gmail.com' AND password_hash = 'test') AS exists;
 
--- auth-tables.sql
+++ auth-tables.sql
@@ -1,29 +1,20 @@
+-- role enum
+DROP TYPE IF EXISTS roles;
+
+CREATE TYPE roles AS ENUM ('admin', 'lecturer', 'student');
+
 -- user table
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users
 (
     id            SERIAL PRIMARY KEY,
-    email         VARCHAR(255)                        NOT NULL,
+    email         VARCHAR(255) UNIQUE                 NOT NULL,
     password_hash CHAR(25)                            NOT NULL,
-    role          INTEGER REFERENCES roles (id)       NOT NULL,
+    role          roles                               NOT NULL,
     created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     access_token  CHAR(25)  DEFAULT NULL
 );
 
 INSERT INTO users (email, password_hash, role)
-VALUES ('test@gmail.com', 'test', 2);
-
--- role table
-DROP TABLE IF EXISTS roles;
-
-CREATE TABLE roles
-(
-    id   SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
-);
-
-INSERT INTO roles (name)
-VALUES ('admin'),
-       ('lecturer'),
-       ('student');
+VALUES ('test@gmail.com', 'test', 'lecturer');
