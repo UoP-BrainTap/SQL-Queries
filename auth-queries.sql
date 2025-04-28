e0629e062e0ef97a5dcf1cae8600ec4c8184e66d
Benjamin Lovering <benjaminlovering1@gmail.com>
2025-04-28 23:12:38 +0100

    Tweaked auth queries

--- auth-queries.sql
+++ auth-queries.sql
@@ -6,19 +6,25 @@ SELECT EXISTS(SELECT 1 FROM users WHERE email = 'test@gmail.com') AS exists;
 WITH existing_user AS (
     SELECT id, email FROM users WHERE email = 'test@example.com'
 )
-INSERT INTO users (email, password_hash, role)
-SELECT 'test@example.com', 'hash', 'lecturer'
+INSERT INTO users (email, password_hash, role, access_token)
+SELECT 'test@example.com', 'hash', 'lecturer', 'token'
 WHERE NOT EXISTS (SELECT 1 FROM existing_user)
 RETURNING id;
 
 -- check if user exists and password is correct
 SELECT EXISTS(SELECT 1 FROM users WHERE email = 'test@gmail.com' AND password_hash = 'test') AS exists;
 
+-- get hashed password of user
+SELECT password_hash
+FROM users
+WHERE email = 'test@gmail.com'
+LIMIT 1;
+
 -- update new access token
 UPDATE users
 SET access_token = 'new_token'
 WHERE email = 'test@gmail.com';
-
+`
 -- check access token exists and return user role and id
 -- 0 rows = access token does not exist
 SELECT id, role
--- auth-tables.sql
+++ auth-tables.sql
@@ -10,10 +10,10 @@ CREATE TABLE users
 (
     id            SERIAL PRIMARY KEY,
     email         VARCHAR(255) UNIQUE                 NOT NULL,
-    password_hash CHAR(25)                            NOT NULL,
+    password_hash TEXT                          NOT NULL,
     role          roles                               NOT NULL,
     created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    access_token  CHAR(25)  DEFAULT NULL
+    access_token  CHAR(32)  DEFAULT NULL
 );
 
 INSERT INTO users (email, password_hash, role)
